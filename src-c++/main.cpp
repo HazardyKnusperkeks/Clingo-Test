a7bb1644dd7409e1227a20dea3aee8aa09583f85
Bjoern Schaepers <bjoern.schaepers@rwth-aachen.de>
2016-08-05 12:35:02 +0200

    Add GUI Skeleton.

--- src-c++/main.cpp
+++ src-c++/main.cpp
@@ -1,4 +1,37 @@
+#include <QApplication>
+#include <QGridLayout>
+#include <QLabel>
+#include <QLineEdit>
+#include <QListView>
+#include <QMessageBox>
+#include <QPushButton>
+#include <QStringListModel>
 
 int main(int argc, char *argv[]) {
-	return 0;
+	QApplication app(argc, argv);
+	
+	QWidget widget;
+	QGridLayout layout(&widget);
+	QLabel status(&widget);
+	QLabel result(&widget);
+	QPushButton start("Start", &widget);
+	QPushButton incr("Incr. t: 0", &widget);
+	QLineEdit edit(&widget);
+	QPushButton add("Add", &widget);
+	QListView view(&widget);
+	QStringListModel model;
+	
+	view.setModel(&model);
+	
+	layout.addWidget(&status, 0, 0);
+	layout.addWidget(&start,  0, 1);
+	layout.addWidget(&incr,   0, 2);
+	layout.addWidget(&result, 1, 0, 1, -1);
+	layout.addWidget(&edit,   2, 0, 1,  2);
+	layout.addWidget(&add,    2, 2);
+	layout.addWidget(&view,   3, 0, 1, -1);
+	
+	widget.show();
+	
+	return app.exec();
 }
